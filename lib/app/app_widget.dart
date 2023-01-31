d2f1fdec51f2dc2d34f1e74ee2e913d20d62b5be
Denin <denersonh@gmail.com>
2023-01-31 18:56:03 -0300

    [develop] Theme configurations and extentions UI.

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'pages/splash/splash.dart';
+import 'core/ui/theme/theme_config.dart';
+
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Calculator',
+    return MaterialApp(
+      title: 'Delivery App',
       debugShowCheckedModeBanner: false,
-      // home: CalculatorPage(),
+      theme: ThemeConfig.theme,
+      routes: {
+        '/': (context) => const SplashPage(),
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'app/app_widget.dart';
 import 'package:flutter/material.dart';
+
+import 'app/app_widget.dart';
 
 void main() {
   runApp(const AppWidget());
